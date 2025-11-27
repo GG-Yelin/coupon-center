ca8e8f2956ed5207a8c0aa426077dbaef83f4588
wangyubj15 <wangyubj15@kanyun.com>
2025-11-27 19:04:28 +0800

    ADD init

---
+++ coupon-customer-server/coupon-customer-impl/src/test/java/org/example/couponcustomerimpl/CouponCustomerImplApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.couponcustomerimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponCustomerImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ coupon-template-server/coupon-template-impl/src/main/java/org/example/coupontemplateimpl/CouponTemplateImplApplication.java
@@ -0,0 +1,13 @@
+package org.example.coupontemplateimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponTemplateImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponTemplateImplApplication.class, args);
+    }
+
+}
