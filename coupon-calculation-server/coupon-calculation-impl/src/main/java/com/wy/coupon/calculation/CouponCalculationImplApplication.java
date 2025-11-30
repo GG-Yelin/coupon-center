ce45f0418fbdb519d1493757c754a49a5d963d4f
wangyu <725383070@qq.com>
2025-11-30 23:27:49 +0800

    INIT

--- coupon-calculation-server/coupon-calculation-impl/src/main/java/com/wy/coupon/calculation/CouponCalculationImplApplication.java
+++ coupon-calculation-server/coupon-calculation-impl/src/main/java/com/wy/coupon/calculation/CouponCalculationImplApplication.java
@@ -2,8 +2,10 @@ package com.wy.coupon.calculation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CouponCalculationImplApplication {
 
     public static void main(String[] args) {
