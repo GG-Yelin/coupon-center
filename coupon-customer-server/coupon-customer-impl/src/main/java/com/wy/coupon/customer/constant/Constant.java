487fa643fc78e934c9a59c7b8b5fc40148ecc7fe
wangyu <725383070@qq.com>
2025-12-02 01:06:47 +0800

    INIT

---
+++ coupon-customer-server/coupon-customer-impl/src/main/java/com/wy/coupon/customer/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.wy.coupon.customer.constant;
+
+public interface Constant {
+
+    String TRAFFIC_VERSION = "traffic-version";
+
+}
---
+++ coupon-customer-server/coupon-customer-impl/src/main/java/com/wy/coupon/customer/loadbalance/CanaryRule.java
@@ -0,0 +1,105 @@
+package com.wy.coupon.customer.loadbalance;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.*;
+import org.springframework.cloud.loadbalancer.core.NoopServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.SelectedInstanceCallback;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static com.wy.coupon.customer.constant.Constant.TRAFFIC_VERSION;
+
+// 可以将这个负载均衡策略单独拎出来，作为一个公共组件提供服务
+@Slf4j
+public class CanaryRule implements ReactorServiceInstanceLoadBalancer {
+
+    private ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider;
+    private String serviceId;
+
+    // 定义一个轮训策略的种子
+    final AtomicInteger position;
+
+    public CanaryRule(ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId) {
+        this.serviceInstanceListSupplierProvider = serviceInstanceListSupplierProvider;
+        this.serviceId = serviceId;
+        position = new AtomicInteger(new Random().nextInt(1000));
+    }
+
+    // 这个服务是Loadbalancer的标准接口，也是负载均衡策略选择服务器的入口方法
+    @Override
+    public Mono<Response<ServiceInstance>> choose(Request request) {
+        ServiceInstanceListSupplier supplier = serviceInstanceListSupplierProvider.getIfAvailable(NoopServiceInstanceListSupplier::new);
+        return supplier.get(request).next().map(serviceInstances -> processInstanceResponse(supplier, serviceInstances, request));
+    }
+
+    // 本方法主要完成了对getInstanceResponse的调用
+    private Response<ServiceInstance> processInstanceResponse(ServiceInstanceListSupplier supplier, List<ServiceInstance> serviceInstances, Request request) {
+        Response<ServiceInstance> serviceInstanceResponse = getInstanceResponse(serviceInstances, request);
+
+        if (supplier instanceof SelectedInstanceCallback && serviceInstanceResponse.hasServer()) {
+            ((SelectedInstanceCallback) supplier).selectedServiceInstance(serviceInstanceResponse.getServer());
+        }
+
+        return serviceInstanceResponse;
+    }
+
+    // 根据金丝雀的规则返回目标节点
+    Response<ServiceInstance> getInstanceResponse(List<ServiceInstance> instances, Request request) {
+        // 注册中心无可用实例 抛出异常
+        if (CollectionUtils.isEmpty(instances)) {
+            log.warn("No instance available {}", serviceId);
+            return new EmptyResponse();
+        }
+        // 从WebClient请求的Header中获取特定的流量打标值
+        // 注意：以下代码仅适用于WebClient调用，使用RestTemplate或者Feign则需要额外适配
+        DefaultRequestContext context = (DefaultRequestContext) request.getContext();
+        RequestData requestData = (RequestData) context.getClientRequest();
+        HttpHeaders headers = requestData.getHeaders();
+        // 获取到header中的流量标记
+        String trafficVersion = headers.getFirst(TRAFFIC_VERSION);
+
+        // 如果没有找到打标标记，或者标记为空，则使用RoundRobin规则进行轮训
+        if (StringUtils.isEmpty(trafficVersion)) {
+            List<ServiceInstance> noneCanaryInstance = instances.stream()
+                    .filter(e -> !e.getMetadata().containsKey(TRAFFIC_VERSION))
+                    .collect(Collectors.toList());
+            return getRoundRobinInstance(noneCanaryInstance);
+        }
+
+        // 找出所有金丝雀服务器，用RoundRobin算法挑出一台
+        List<ServiceInstance> canaryInstance = instances.stream()
+                .filter(e -> {
+                    String trafficVersionInMetadata = e.getMetadata().get(TRAFFIC_VERSION);
+                    return StringUtils.equals(trafficVersionInMetadata, trafficVersion);
+                }).collect(Collectors.toList());
+
+        return getRoundRobinInstance(canaryInstance);
+
+    }
+
+    // 使用RoundRobin机制获取节点
+    private Response<ServiceInstance> getRoundRobinInstance(List<ServiceInstance> instances) {
+        if (CollectionUtils.isEmpty(instances)) {
+            log.warn("No servers available for service: " + serviceId);
+            return new EmptyResponse();
+        }
+
+        // 每一次计数器都自动+1，实现轮询的效果
+        int pos = Math.abs(this.position.incrementAndGet());
+        ServiceInstance instance = instances.get(pos % instances.size());
+        return new DefaultResponse(instance);
+    }
+
+}
